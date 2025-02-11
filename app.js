b20cb4da661314e7150bf6f36f7fed130b48b4f2
Nguyen Huu Duc Admin <nguyenhuuduc@Mac.lan>
2025-02-12 00:15:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 // Khoi tao ung dung, mot the hien cua express
 const app = express();
 
@@ -17,7 +19,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Wellcome to contact book application." });
 })
 
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server Error",
+    });
+});
+
+
 //Xuat module app de co the su dung o file khac
 module.exports = app;
-
-
