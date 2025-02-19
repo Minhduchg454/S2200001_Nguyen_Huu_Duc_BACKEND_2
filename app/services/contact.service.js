f75a3e215f425048c7328b9b806da212780bd5c1
Nguyen Huu Duc <minhduchg454@gmail.com>
2025-02-19 16:22:16 +0700

    new

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -1,9 +1,37 @@
-const { ObjectId } = require('mongodb');
+const { ObjectId, ReturnDocument } = require('mongodb');
 
 class ContactService {
     constructor(client){
         this.Contact = client.db().collection('contacts');
     }
+
+    extractContactData (payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+
+        Object.keys(contact).forEach(
+            key => contact[key] === undefined && delete contact[key]
+        );
+        return contact;    
+
+    }
+
+    async create(payload){
+        const contact = this.extractContactDat(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: {favorite: contact.favorite == true} },
+            { ReturnDocument: "after", upsert: true }
+        );
+        return result;
+    }
+
+
 
 }
 
